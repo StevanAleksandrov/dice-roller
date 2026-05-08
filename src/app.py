bfd714dd2775f35fa03251f183cc18217ecf8163
Stevan Aleksandrov <stevanaleksandrov@gmail.com>
2026-05-08 14:53:06 +0200

    chore: add flask templates and layout code

--- src/app.py
+++ src/app.py
@@ -1,8 +1,20 @@
-# save this as app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/<string:name>")
+def hello_world(name: str = None):
+    return render_template("hello.html", _name=name)
+
+# @app.route("/<name>")
+# def personalized_hello(name):
+#     return render_template("hello.html", _name=name)
+
+@app.route("/dices")
+def handle_dices():
+    return "look at these beautiful dices"
+
+# @app.route("/motivation")
+# def motivation_txt():
+#     return "Push through now, and you'll thank yourself later."
